8efc69bd8c3ddb117d5560b384d04f0c02bfa887
Christoph Rumpel <c.rumpel@kabsi.at>
2015-05-09 23:20:39 +0200

    change some text

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -28,7 +28,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="{{ route('game.home') }}">My favourite Marvel character</a>
+				<a class="navbar-brand" href="{{ route('game.home') }}">My favourite THE Marvel character</a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
