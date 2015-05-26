10ccb8752339ae351f4e411348d218ff4177c862
Christoph Rumpel <c.rumpel@kabsi.at>
2015-05-26 21:05:29 +0200

    Change image format and size

--- resources/views/parts/character.blade.php
+++ resources/views/parts/character.blade.php
@@ -1,6 +1,6 @@
 <div class="thumbnail">
 
-    <img src="{{ $character['thumbnail']['path'] . '.' . $character['thumbnail']['extension'] }}"/>
+    <img src="{{ $character['thumbnail']['path'] . '/portrait_uncanny.' . $character['thumbnail']['extension'] }}"/>
 
     <div class="caption">
         <h3>{{ $character['name'] }}</h3>
